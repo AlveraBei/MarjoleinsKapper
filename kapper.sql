0053b56f4635cb12ee66bb5b44e8b063bfb31d17
playtheghost <muhamedshamsaddin@gmail.com>
2022-11-22 09:03:46 +0100

    s

--- kapper.sql
+++ kapper.sql
@@ -1,179 +1,108 @@
--- MariaDB dump 10.19  Distrib 10.4.24-MariaDB, for Win64 (AMD64)
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
 --
--- Host: localhost    Database: kapper
--- ------------------------------------------------------
--- Server version	10.4.24-MariaDB
+-- Host: 127.0.0.1
+-- Generation Time: Nov 22, 2022 at 09:03 AM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Database: `kapper`
+--
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `afspraakkt`
 --
 
-DROP TABLE IF EXISTS `afspraakkt`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `afspraakkt` (
   `afspraak_id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  UNIQUE KEY `afspraak_id` (`afspraak_id`),
-  UNIQUE KEY `user_id` (`user_id`)
+  `user_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `afspraakkt`
---
-
-LOCK TABLES `afspraakkt` WRITE;
-/*!40000 ALTER TABLE `afspraakkt` DISABLE KEYS */;
-/*!40000 ALTER TABLE `afspraakkt` ENABLE KEYS */;
-UNLOCK TABLES;
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `afspraken`
 --
 
-DROP TABLE IF EXISTS `afspraken`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `afspraken` (
   `id` int(11) NOT NULL,
   `klanten_id` int(11) NOT NULL,
-  `datum` date NOT NULL,
-  UNIQUE KEY `klanten_id` (`klanten_id`)
+  `datum` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `afspraken`
---
-
-LOCK TABLES `afspraken` WRITE;
-/*!40000 ALTER TABLE `afspraken` DISABLE KEYS */;
-/*!40000 ALTER TABLE `afspraken` ENABLE KEYS */;
-UNLOCK TABLES;
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `klanten`
 --
 
-DROP TABLE IF EXISTS `klanten`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `klanten` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `naam` varchar(50) NOT NULL,
   `email` varchar(50) NOT NULL,
   `telefoon` int(13) NOT NULL,
   `afspraak` date NOT NULL,
   `geslacht` varchar(10) NOT NULL,
   `noties` varchar(255) NOT NULL,
-  `praat` tinyint(1) NOT NULL,
-  PRIMARY KEY (`id`)
+  `praat` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `klanten`
---
-
-LOCK TABLES `klanten` WRITE;
-/*!40000 ALTER TABLE `klanten` DISABLE KEYS */;
-/*!40000 ALTER TABLE `klanten` ENABLE KEYS */;
-UNLOCK TABLES;
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `servicekt`
 --
 
-DROP TABLE IF EXISTS `servicekt`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `servicekt` (
   `afspraak_id` int(11) NOT NULL,
-  `service_id` int(11) NOT NULL,
-  UNIQUE KEY `afspraak_id` (`afspraak_id`),
-  UNIQUE KEY `service_id` (`service_id`)
+  `service_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `servicekt`
---
-
-LOCK TABLES `servicekt` WRITE;
-/*!40000 ALTER TABLE `servicekt` DISABLE KEYS */;
-/*!40000 ALTER TABLE `servicekt` ENABLE KEYS */;
-UNLOCK TABLES;
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `services`
 --
 
-DROP TABLE IF EXISTS `services`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `services` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
   `serviceduur` int(11) NOT NULL,
   `serviceprijs` decimal(50,0) NOT NULL,
-  `servicenaam` varchar(50) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `user_id` (`user_id`)
+  `servicenaam` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `services`
---
-
-LOCK TABLES `services` WRITE;
-/*!40000 ALTER TABLE `services` DISABLE KEYS */;
-/*!40000 ALTER TABLE `services` ENABLE KEYS */;
-UNLOCK TABLES;
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `userkt`
 --
 
-DROP TABLE IF EXISTS `userkt`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `userkt` (
   `afspraak_id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  UNIQUE KEY `afspraak_id` (`afspraak_id`),
-  UNIQUE KEY `user_id` (`user_id`)
+  `user_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `userkt`
---
-
-LOCK TABLES `userkt` WRITE;
-/*!40000 ALTER TABLE `userkt` DISABLE KEYS */;
-/*!40000 ALTER TABLE `userkt` ENABLE KEYS */;
-UNLOCK TABLES;
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `users`
 --
 
-DROP TABLE IF EXISTS `users`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `rol` varchar(50) NOT NULL,
@@ -182,24 +111,118 @@ CREATE TABLE `users` (
   `wachtwoord` varchar(50) NOT NULL,
   `kapperid` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `users`
+-- Indexes for dumped tables
 --
 
-LOCK TABLES `users` WRITE;
-/*!40000 ALTER TABLE `users` DISABLE KEYS */;
-/*!40000 ALTER TABLE `users` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+--
+-- Indexes for table `afspraakkt`
+--
+ALTER TABLE `afspraakkt`
+  ADD UNIQUE KEY `afspraak_id` (`afspraak_id`),
+  ADD UNIQUE KEY `user_id` (`user_id`);
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+--
+-- Indexes for table `afspraken`
+--
+ALTER TABLE `afspraken`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `klanten_id` (`klanten_id`);
+
+--
+-- Indexes for table `klanten`
+--
+ALTER TABLE `klanten`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `servicekt`
+--
+ALTER TABLE `servicekt`
+  ADD UNIQUE KEY `afspraak_id` (`afspraak_id`),
+  ADD UNIQUE KEY `service_id` (`service_id`);
+
+--
+-- Indexes for table `services`
+--
+ALTER TABLE `services`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `userkt`
+--
+ALTER TABLE `userkt`
+  ADD UNIQUE KEY `afspraak_id` (`afspraak_id`),
+  ADD UNIQUE KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `afspraken`
+--
+ALTER TABLE `afspraken`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `klanten`
+--
+ALTER TABLE `klanten`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `services`
+--
+ALTER TABLE `services`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `afspraakkt`
+--
+ALTER TABLE `afspraakkt`
+  ADD CONSTRAINT `afspraakkt_ibfk_1` FOREIGN KEY (`afspraak_id`) REFERENCES `afspraken` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `afspraakkt_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `afspraken`
+--
+ALTER TABLE `afspraken`
+  ADD CONSTRAINT `afspraken_ibfk_1` FOREIGN KEY (`klanten_id`) REFERENCES `klanten` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `servicekt`
+--
+ALTER TABLE `servicekt`
+  ADD CONSTRAINT `servicekt_ibfk_1` FOREIGN KEY (`service_id`) REFERENCES `services` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `servicekt_ibfk_2` FOREIGN KEY (`afspraak_id`) REFERENCES `afspraken` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `services`
+--
+ALTER TABLE `services`
+  ADD CONSTRAINT `services_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `userkt`
+--
+ALTER TABLE `userkt`
+  ADD CONSTRAINT `userkt_ibfk_1` FOREIGN KEY (`afspraak_id`) REFERENCES `afspraken` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `userkt_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2022-11-21 14:32:31
