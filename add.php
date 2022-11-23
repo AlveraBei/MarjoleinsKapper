6fffe480636fab098395fb8c470cc591d22e5f32
ThijmenH01 <thijmenh01@gmail.com>
2022-11-23 11:19:41 +0100

    a

--- add.php
+++ add.php
@@ -1,1 +1,14 @@
 <?php
+include("login/db.php");
+$sql = "INSERT INTO `klanten`(`id`, `naam`, `email`, `telefoon`, `afspraak`, `geslacht`, `noties`, `praat`) VALUES ('$id', '$naam', '$email', '$telefoon', '$afspraak', '$geslacht', '$notites', '$praat')";
+$conn->exec($sql);
+
+
+try {
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $sql = "INSERT INTO `klanten`(`id`, `naam`, `email`, `telefoon`, `afspraak`, `geslacht`, `noties`, `praat`) VALUES ('$id', '$naam', '$email', '$telefoon', '$afspraak', '$geslacht', '$notites', '$praat')";
+    $conn->exec($sql);
+    echo "New record created successfully";
+} catch (PDOException $e) {
+    echo $sql . "<br>" . $e->getMessage();
+}
